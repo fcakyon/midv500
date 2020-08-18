906e02d86c7cf621d90c1235b91870535b7c7318
fcakyon <fcakyon@gmail.com>
2020-08-18 13:32:46 +0300

    update tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -43,6 +43,7 @@ class ConverterTests(unittest.TestCase):
         # read annotations paths
         annotation_paths = list_annotation_paths_recursively(directory, ignore_background_only_ones=True)
         self.assertEqual(len(annotation_paths), 90)
+        self.assertEqual(annotation_paths[3], '01_alb_id/ground_truth/HA/HA01_04.json')
 
     def test_download(self):
         from midv500.utils import download
